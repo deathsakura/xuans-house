5dd713ec6e2ad71d3843bb3d73b8b63cc22779ac
yuxuan.han <yuxuan.han@THINKPAD-T460>
2018-05-22 13:26:12 +0800

    add fields

--- model/src/main/java/club/yuxuan/yun/model/account/Account.java
+++ model/src/main/java/club/yuxuan/yun/model/account/Account.java
@@ -1,10 +1,14 @@
 package club.yuxuan.yun.model.account;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @description 账号
  *
@@ -17,19 +21,57 @@ import java.io.Serializable;
 public class Account implements Serializable {
 
     private static final long serialVersionUID = -7795804396904895045L;
-    
+
+    /**
+     * ID
+     */
+    @ApiModelProperty("ID")
     private String id;
-    
+
+    /**
+     * 用户名
+     */
+    @ApiModelProperty("用户名")
     private String username;
 
+    /**
+     * 密码
+     */
+    @ApiModelProperty("密码")
     private String password;
-
+    
+    /**
+     * 邮箱
+     */
+    @ApiModelProperty("邮箱")
     private String email;
-    
+
+    /**
+     * 手机
+     */
+    @ApiModelProperty("手机")
     private String phone;
-    
+
+    /**
+     * 是否实名
+     */
+    @ApiModelProperty("是否实名")
     private Boolean realName;
-    
+
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty("创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDate;
+
+    /**
+     * 更新时间
+     */
+    @ApiModelProperty("更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateDate;
+
     @Override
     public String toString() {
         return "Account{" +
@@ -37,6 +79,10 @@ public class Account implements Serializable {
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", realName=" + realName +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
                 '}';
     }
 }
