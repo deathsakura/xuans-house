1701cd0f7497bc2fedca7eff595c718eca1e8d0d
yuxuan.han <yuxuan_java@163.com>
2018-07-26 09:47:43 +0800

    2018-07-26 9:46:51

--- model/src/main/java/club/yuxuan/yun/model/Java8Tester.java
+++ model/src/main/java/club/yuxuan/yun/model/Java8Tester.java
@@ -11,10 +11,22 @@ public class Java8Tester {
         };
         greetingService.sayMessage("aaa");
         greetingService.sayMessage("bbb");
+        
+        TestService testService = (a, b, c) -> {
+            System.out.println("a: " + a);
+            System.out.println("b: " + b);
+            System.out.println("c: " + c);
+        };
+        
+        testService.methodA("1", "2", 3);
     }
     
     interface GreetingService {
         void sayMessage(String message);
+    }
+    
+    interface TestService {
+        void methodA(String a, String b, Integer c);
     }
     
 }
